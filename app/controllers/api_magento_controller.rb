9526d8dfef01e9541280b4a81bae75a228b81f9f
rem <kremartem@gmail.com>
2016-07-19 19:03:30 +0300

    1

--- app/controllers/api_magento_controller.rb
+++ app/controllers/api_magento_controller.rb
@@ -1,32 +1,33 @@
 class ApiMagentoController < ApplicationController
   def autorization
+    #отримання token
     result = RestClient.post "http://demo.beta.qpard.com/index.php/rest/V1/integration/admin/token", {"username":"qpard", "password":"qwerty_123456"}.to_json,
                              :content_type => :json, :accept => :json
 
-    time_start = Time.now
-    1000.times do |i|
-      begin
-
-        req = RestClient.put "http://demo.beta.qpard.com/index.php/rest/V1/products/10090-White-XL#{i}",
-                              {
-                                  "product":{
-                                      "sku": "10090-White-XL#{i}",
-                                      "name": "10090-White-XL#{i}",
-                                      "attribute_set_id": 4,
-                                      "price": i,
-                                      "status": 1,
-                                      "visibility": 1,
-                                      "type_id": "simple" }
-                              }.to_json,
-                              {
-                                  :Authorization => "Bearer #{result.to_s.gsub('"','')}",
-                                  :content_type => :json,
-                                  :accept => :json
-                              }
-      rescue => error
-        a=2
-      end
-
+    # time_start = Time.now
+    # 1000.times do |i|
+    #   begin
+    #
+    #     req = RestClient.put "http://demo.beta.qpard.com/index.php/rest/V1/products/10090-White-XL#{i}",
+    #                           {
+    #                               "product":{
+    #                                   "sku": "10090-White-XL#{i}",
+    #                                   "name": "10090-White-XL#{i}",
+    #                                   "attribute_set_id": 4,
+    #                                   "price": i,
+    #                                   "status": 1,
+    #                                   "visibility": 1,
+    #                                   "type_id": "simple" }
+    #                           }.to_json,
+    #                           {
+    #                               :Authorization => "Bearer #{result.to_s.gsub('"','')}",
+    #                               :content_type => :json,
+    #                               :accept => :json
+    #                           }
+    #   rescue => error
+    #     a=2
+    #   end
+    #
 
 
 
@@ -50,10 +51,11 @@ class ApiMagentoController < ApplicationController
       # rescue => error
       #   a=2
       # end
-    end
-    time_end = Time.now
-    result_time = time_end - time_start
+    # end
+    # time_end = Time.now
+    # result_time = time_end - time_start
     a=2
+
 
   end
 end
